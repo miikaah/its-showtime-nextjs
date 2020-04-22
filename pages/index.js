2965b40a0ee614c29bc5d3b6a9b35996458bd13a
Miika Henttonen <miika.henttonen@wunderdog.fi>
2020-04-22 11:47:08 +0300

    Add some colors

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,45 @@
 import Head from "next/head";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --color-white: #fbfbfb;
+    --color-black: #000;
+    --color-base: #002525;
+    --color-base-light: #093a3a;
+    --color-base-dark: #001414;
+    --color-highlight: #ff0303;
+    --color-highlight2: #ffab03;
+  }
+
+  html, body {
+    padding: 0;
+    margin: 0;
+  }
+
+  html {
+    background-image: linear-gradient(#093a3a, #001414);
+    height: 100vh;
+  }
+`;
 
 const Text = styled.div`
-  background-color: red;
-  color: white;
+  background-color: ${({ hasBg }) =>
+    hasBg ? "var(--color-highlight2)" : "transparent"};
+  color: var(--color-white);
 `;
 
 export default function Home() {
   return (
     <>
+      <GlobalStyle />
       <Head>
         <title>It&apos;s Showtime!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Text>Hello</Text>
+      <Text hasBg>Hello</Text>
+      <Text>World</Text>
     </>
   );
 }
