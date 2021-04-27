8d06e9352220bae42bb418c36ff7de5c9ee8546e
Miika Henttonen <miika.henttonen@wunderdog.fi>
2021-04-27 11:38:09 +0300

    Enable webpack5 for nextJs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require("next-pwa");
 
 const isDev = process.env.NODE_ENV === "development";
 
 module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    disable: isDev,
-    register: !isDev,
-  },
+    future: {
+        webpack5: true,
+    },
+    pwa: {
+        dest: "public",
+        disable: isDev,
+        register: !isDev,
+    },
 });
